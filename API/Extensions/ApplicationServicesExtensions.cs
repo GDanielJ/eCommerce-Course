d128404678db2b3b12cba54c2e75273a4ed63db7
Daniel Johansson <johansson.gd@gmail.com>
2020-12-15 19:26:42 +0100

    klar med sektion 5

---
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using API.Errors;
+
+namespace API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            { // This needs to be added AFTER services.AddControllers();
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                         .Where(e => e.Value.Errors.Count > 0)
+                         .SelectMany(x => x.Value.Errors)
+                         .Select(x => x.ErrorMessage).ToArray();
+                    var errorResponse = new ApiValidationErrorResponse
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            return services;
+        }
+    }
+}
