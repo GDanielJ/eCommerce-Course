f6da2940972c79806bcab1eda76a54a438dae86b
Daniel Johansson <johansson.gd@gmail.com>
2021-01-11 19:10:24 +0100

    klar med 11.118

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -29,9 +29,9 @@ namespace Infrastructure.Data
                 {
                     var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
 
-                    foreach (var propterty in properties)
+                    foreach (var property in properties)
                     {
-                        modelBuilder.Entity(entityType.Name).Property(propterty.Name).HasConversion<double>();
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
                     }
                 }
             }
