d8c2db9c6afebe709463b546a905b8facde2082d
Daniel Johansson <johansson.gd@gmail.com>
2021-01-25 18:03:27 +0100

    klar med 14.156

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IBasket } from '../shared/Models/basket';
+import { IBasket, IBasketItem } from '../shared/Models/basket';
 import { BasketService } from './basket.service';
 import { Observable } from 'rxjs';
 
@@ -15,6 +15,18 @@ export class BasketComponent implements OnInit {
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+  }
+
+  removeBasketItem(item: IBasketItem) {
+    this.basketService.removeItemFromBasket(item);
+  }
+
+  incrementItemQuantity(item: IBasketItem) {
+    this.basketService.incrementItemQuantity(item);
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    this.basketService.decrementItemQuantity(item);
   }
 
 }
