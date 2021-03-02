7e85e53131553fbe80c5b1a6d8db6a2f606038d2
Daniel Johansson <johansson.gd@gmail.com>
2021-03-02 17:55:53 +0100

    klar med 18.208

---
+++ Core/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Core.Entities.OrderAggregate
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value = "Pending")]
+        Pending,
+        [EnumMember(Value = "Payment recieved")]
+        PaymentRecieved,
+        [EnumMember(Value = "Payment failed")]
+        PaymentFailed
+    }
+}
