d8909ac555d37bdd0bb55dd80161ddfce2907418
Daniel Johansson <johansson.gd@gmail.com>
2021-03-12 18:07:09 +0100

    klar med 19.238

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -10,7 +10,9 @@ import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
+import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -27,7 +29,8 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptors';
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
