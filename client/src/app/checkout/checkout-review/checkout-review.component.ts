ebf0f7e39da011a9c285fcce88e78ccf3762f047
Daniel Johansson <johansson.gd@gmail.com>
2021-03-22 18:47:55 +0100

    klar med 21.265

--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IBasket } from '../../shared/Models/basket';
+import { BasketService } from '../../basket/basket.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-checkout-review',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-review.component.scss']
 })
 export class CheckoutReviewComponent implements OnInit {
+  basket$: Observable<IBasket>;
 
-  constructor() { }
+  constructor(private basketService: BasketService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  createPaymentIntent() {
+    return this.basketService.createPaymentIntent().subscribe((response: any) => {
+      this.toastr.success('Payment intent created');
+    }, error => {
+      console.log(error);
+      this.toastr.error(error.message);
+    });
   }
 
 }
